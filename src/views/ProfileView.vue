85e9266d5cfe04c510a9be4171369536bfedef22
이응주(Eung Ju Lee) <eungjulee77@gmail.com>
2023-06-08 17:41:47 +0900

    [#5]레이아웃 변경

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -7,36 +7,36 @@ export default {
 
 <template>
   <div class="container text-center">
-    <div class="row">
-      <div class="col-3" >
-        <div class="row-9">
-          <img src="https://play-lh.googleusercontent.com/aFWiT2lTa9CYBpyPjfgfNHd0r5puwKRGj2rHpdPTNrz2N9LXgN_MbLjePd1OTc0E8Rl1=w240-h480-rw">
+    <div class="row justify-content-center">
+      <!-- 프로필 사진 -->
+      <div class="col-md-4" >
+        <div class="row-9 mt-3">
+          <img class="border border-1" src="https://play-lh.googleusercontent.com/aFWiT2lTa9CYBpyPjfgfNHd0r5puwKRGj2rHpdPTNrz2N9LXgN_MbLjePd1OTc0E8Rl1=w240-h480-rw">
         </div>
-        <div class="row-3">
+        <div class="row-3 mt-3">
           <button type="button" class="btn btn-primary">사진변경</button>
         </div>
       </div>
-      <div class="col-4">
-        <div class="input-group mt-3">
-          <span class="input-group-text justify-content-center" style="width:100px">아이디</span>
-          <input type="text" class="form-control no-select" aria-label="아이디" aria-describedby="basic-addon1">
+
+      <!-- 사용자 정보-->
+      <div class="col-md-4 ">
+        <div class="row">
+          <div class="col-4">
+            <span class="input-group-text justify-content-center mt-3">아이디</span>
+            <span class="input-group-text justify-content-center mt-3">이름</span>
+            <span class="input-group-text justify-content-center mt-3">팀</span>
+            <span class="input-group-text justify-content-center mt-3">이메일</span>
+            <span class="input-group-text justify-content-center mt-3">전화번호</span>
+          </div>
+          <div class="col-8">
+            <input type="text" class="form-control no-select mt-3">
+            <input type="text" class="form-control no-select mt-3">
+            <input type="text" class="form-control no-select mt-3">
+            <input type="email" class="form-control no-select mt-3">
+            <input type="text" class="form-control no-select mt-3">
+          </div>
         </div>
-        <div class="input-group mt-3">
-          <span class="input-group-text justify-content-center" style="width:100px">이름</span>
-          <input type="text" class="form-control no-select" aria-label="이름" aria-describedby="basic-addon1">
-        </div>
-        <div class="input-group mt-3">
-          <span class="input-group-text justify-content-center" style="width:100px">팀</span>
-          <input type="text" class="form-control no-select" aria-label="팀" aria-describedby="basic-addon1">
-        </div>
-        <div class="input-group mt-3">
-          <span class="input-group-text justify-content-center" style="width:100px">이메일</span>
-          <input type="text" class="form-control no-select" aria-label="이메일" aria-describedby="basic-addon1">
-        </div>
-        <div class="input-group mt-3">
-          <span class="input-group-text justify-content-center" style="width:100px">전화번호</span>
-          <input type="text" class="form-control no-select" aria-label="전화번호" aria-describedby="basic-addon1">
-        </div>
+        
         <div class="mt-3">
           <button type="button" class="btn btn-primary">정보수정</button>
         </div>
