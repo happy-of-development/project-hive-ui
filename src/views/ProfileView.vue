59ca68aea3752fa1df73afc30a56b071075edae9
이응주(Eung Ju Lee) <eungjulee77@gmail.com>
2023-06-16 18:40:30 +0900

    [#5] axios를 이용해서버 접속테스트 코드작성

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,20 +1,63 @@
 <script>
+import axios from 'axios'
+
 export default {
+  
   data() {
     return {
+      orgProfile : {
+        id : '11111111',
+        name : '닉네임',
+        team : '우리팀',
+        email : 'test@test.com',
+        mobile : '010-1111-1111'
+      },
+      profile : {
+        id : '',
+        name : '',
+        team : '',
+        email : '',
+        mobile : ''
+      },
+      password : '',
+      reenterPassword : '',
       modifyMode : false
     }
+  },  
+  mounted() {
+    // 객체를 복사함. this.orgProfile로 직접 assign하면 객체 참조가 됨.
+    this.fetchData();
   },
   methods : {
+      fetchData() {
+        axios.get('http://localhost:9080/user?id=11111111', {
+          headers: {
+            'x-auth-token': '48e16eae-6a8a-42ba-85f1-1a405c500b70'
+          }
+        })
+        .then(res=>{
+          console.log(res.data)
+          console.log(res.data.data.teamName)
+          this.orgProfile.team = res.data.data.teamName;
+
+          this.profile = {...this.orgProfile}
+        })
+        .catch(function(error) {
+          console.log(error)
+        });
+      },
       onModifyClick() {
-        this.modifyMode = true;
+        this.modifyMode = true
       },
       onConfirmClick() {
-        
+
+        this.modifyMode = false
       },
       onCancelClick() {
+        this.profile = {...this.orgProfile}
+        this.password = ''
+        this.reenterPassword = ''
         this.modifyMode = false;
-        
       }
     }
   }
@@ -48,13 +91,13 @@ export default {
             <span id="reenter-password-label" class="input-group-text justify-content-center mt-3" v-bind:class="{'d-none' : !modifyMode}">비밀번호확인</span>
           </div>
           <div class="col-8">
-            <input type="text" class="form-control no-select mt-3">
-            <input id="name" type="text" class="form-control mt-3" v-bind:class="{'no-select' : !modifyMode}">
-            <input type="text" class="form-control no-select mt-3">
-            <input id="email" type="email" class="form-control mt-3" v-bind:class="{'no-select' : !modifyMode}">
-            <input id="mobile" type="text" class="form-control mt-3" v-bind:class="{'no-select' : !modifyMode}">
-            <input id="password" type="password" class="form-control mt-3" v-bind:class="{'d-none' : !modifyMode}">
-            <input id="reenter-password" type="password" class="form-control mt-3" v-bind:class="{'d-none' : !modifyMode}">
+            <input type="text" class="form-control no-select mt-3" v-model="profile.id">
+            <input id="name" type="text" class="form-control mt-3" v-bind:class="{'no-select' : !modifyMode}" v-model="profile.name">
+            <input type="text" class="form-control no-select mt-3" v-model="profile.team">
+            <input id="email" type="email" class="form-control mt-3" v-bind:class="{'no-select' : !modifyMode}" v-model="profile.email">
+            <input id="mobile" type="text" class="form-control mt-3" v-bind:class="{'no-select' : !modifyMode}" v-model="profile.mobile">
+            <input id="password" type="password" class="form-control mt-3" v-bind:class="{'d-none' : !modifyMode}" v-model="password">
+            <input id="reenter-password" type="password" class="form-control mt-3" v-bind:class="{'d-none' : !modifyMode}" v-model="reenterPassword">
           </div>
         </div>
         
