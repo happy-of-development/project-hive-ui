78b19138a23a43c1d9e24e3795dcb71d67b2ded5
이응주(Eung Ju Lee) <eungjulee77@gmail.com>
2023-07-06 18:20:21 +0900

    [#5]Profile 수정 기능 완료

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -10,11 +10,11 @@ export default {
   data() {
     return {
       orgProfile : {
-        id : '11111111',
-        name : '닉네임',
-        teamName : '우리팀',
-        email : 'test@test.com',
-        mobile : '010-1111-1111'
+        id : '',
+        name : '',
+        teamName : '',
+        email : '',
+        mobile : ''
       },
       profile : {
         id : '',
@@ -50,49 +50,63 @@ export default {
       onModifyClick() {
         this.modifyMode = true
       },
-      confirmSubmit() {
-        console.log("confirmSubmit 1")
-        if(this.password !== this.reenterPassword) {
-          console.log('비밀 번호와 재입력 비밀 번호가 맞지 않습니다.')
-        } else {
-          const entity = {id:this.profile.id,
-            name:this.profile.name,
-            mobile:this.profile.mobile,
-            email:this.profile.email, }
+      onSubmit(event) {
+        event.preventDefault()
 
-          if(password !== '') {
-            entity.password = this.password
-          }
-
-          userService.putUser(entity)
-          .then(res=>{
-            if(res.data.code === '200') {              
-             this.fetchData()
-             this.password = ''
-             this.reenterPassword = ''
-            } else {
-              console.log("error code : "  + res.data.code)
-            }
-          })
-          .catch(function(error) {
-            console.log(error)
-          });
-
+        if(JSON.stringify(this.profile) === JSON.stringify(this.orgProfile) && this.password === '') {
           this.modifyMode = false
+          return
         }
+
+        const entity = {id:this.profile.id,
+          name:this.profile.name,
+          mobile:this.profile.mobile,
+          email:this.profile.email, 
+          password: this.password !== ''?this.password:undefined,}
+
+        userService.putUser(entity)
+        .then(res=>{
+          if(res.data.code === '200') {              
+            this.fetchData()
+            this.password = ''
+            this.reenterPassword = ''
+          } else {
+            console.log("error code : "  + res.data.code)
+          }
+        })
+        .catch(function(error) {
+          console.log(error)
+        });
+
+        this.modifyMode = false  
       },
       onCancelClick() {
         this.profile = {...this.orgProfile}
         this.password = ''
         this.reenterPassword = ''
         this.modifyMode = false;
+      },      
+    },
+    computed: {
+      nameIsValid() {        
+        return !this.modifyMode || !!this.profile.name
+      },
+      emailIsValid() {
+        return !this.modifyMode || !!this.profile.email
+      },
+      mobileIsValid() {
+        return !this.modifyMode || !!this.profile.mobile
+      },
+      passwordIsValid() {
+        return !this.modifyMode || (this.password === this.reenterPassword)
+      },
+      formIsValid() {
+        return this.nameIsValid && this.emailIsValid && this.mobileIsValid && this.passwordIsValid
       }
     }
   }
 
 </script>
-
-
 
 <template>
   <div class="container text-center">
@@ -108,60 +122,68 @@ export default {
       </div>
 
       <!-- 사용자 정보-->
-      <form class="needs-validation col-md-4 novalidate" v-on:submit="confirmSubmit">
+      <form class="col-md-4"  @submit="onSubmit" novalidate>
         <div class="row">
-          <div class="col-12 mt-3">
+          <div class="col-12 mt-4 mb-4">
             <div class="input-group">
               <span class = "input-group-text justify-content-center col-4">아이디</span>
               <input type="text" class="form-control no-select" v-model="profile.id" disabled>
             </div>
           </div>
-          <div class="col-12 mt-3">
-            <div class="input-group has-validation">
+          <div class="col-12">
+            <div class="input-group">
               <span class = "input-group-text justify-content-center col-4" >이름</span>
-              <input type="text" class="form-control" id="name" v-bind:class="{'no-select' : !modifyMode}" v-model="profile.name" required>
-              <div class="invalid-feedback">이름을 입력해 주세요.</div>
+              <input type="text" class="form-control" id="name" v-bind:class="{'no-select' : !modifyMode}" v-model="profile.name">              
+            </div>
+            <div class="col-12justify-content-center" style="height: 1.5rem">
+              <div class="text-danger small" v-if="!nameIsValid" >이름을 입력해 주세요.</div> 
             </div>
           </div>
-          <div class="col-12 mt-3">
+          <div class="col-12 mb-4">
             <div class="input-group">
               <span class = "input-group-text justify-content-center col-4" >팀</span>
               <input type="text" class="form-control no-select"  v-model="profile.teamName" disabled>
             </div>
           </div>
-          <div class="col-12 mt-3">
-            <div class="input-group has-validation">
+          <div class="col-12">
+            <div class="input-group">
               <span class = "input-group-text justify-content-center col-4" >이메일</span>
-              <input type="email" class="form-control" id="email" v-bind:class="{'no-select' : !modifyMode}" v-model="profile.email" required>
-              <div class="invalid-feedback">이메일을 입력해 주세요.</div>
+              <input type="email" class="form-control" id="email" v-bind:class="{'no-select' : !modifyMode}" v-model="profile.email">
+            </div>
+            <div class="col-12justify-content-center" style="height: 1.5rem">
+              <div class="text-danger small" v-if="!emailIsValid" >이메일을 입력해 주세요.</div> 
             </div>
           </div>
-          <div class="col-12 mt-3">
-            <div class="input-group has-validation">
+          <div class="col-12">
+            <div class="input-group">
               <span class = "input-group-text justify-content-center col-4" >전화번호</span>
-              <input type="text" class="form-control" id="mobile" v-bind:class="{'no-select' : !modifyMode}" v-model="profile.mobile" required>
-              <div class="invalid-feedback">전화번호를 입력해 주세요.</div>
+              <input type="text" class="form-control" id="mobile" v-bind:class="{'no-select' : !modifyMode}" v-model="profile.mobile">
+            </div>
+            <div class="col-12justify-content-center" style="height: 1.5rem">
+              <div class="text-danger small" v-if="!mobileIsValid">전화번호를 입력해 주세요.</div> 
             </div>
           </div>
-          <div class="col-12 mt-3">
+          <div class="col-12">
             <div class="input-group">
               <span class = "input-group-text justify-content-center col-4" v-bind:class="{'d-none' : !modifyMode}" >비밀번호</span>
-              <input type="text" class="form-control" id="password"  v-bind:class="{'d-none' : !modifyMode}" v-model="password">
+              <input type="password" class="form-control" id="password"  v-bind:class="{'d-none' : !modifyMode}" v-model="password">
             </div>
+            <div class="col-12justify-content-center" style="height: 1.5rem" v-bind:class="{'d-none' : !modifyMode}"></div>
           </div>
-          <div class="col-12 mt-3">
-            <div class="input-group has-validation">
+          <div class="col-12">
+            <div class="input-group">
               <span class = "input-group-text justify-content-center col-4" v-bind:class="{'d-none' : !modifyMode}">비밀번호확인</span>
-              <input type="text" class="form-control" id="reenterPassword" v-bind:class="{'d-none' : !modifyMode}" v-model="reenterPassword">
-              <div class="invalid-feedback">입력한 비밀번호가 다릅니다.</div>
+              <input type="password" class="form-control" id="reenterPassword" v-bind:class="{'d-none' : !modifyMode}" v-model="reenterPassword">
+            </div>
+            <div class="col-12justify-content-center" style="height: 1.5rem" v-bind:class="{'d-none' : !modifyMode}">
+              <div class="text-danger small" v-if="!passwordIsValid">비밀번호가 다릅니다.</div> 
             </div>
           </div>
-        </div>
-        
+        </div>        
         <div>
-          <button id="modify" type="button" class="btn btn-primary m-3" v-bind:class="{'d-none' : modifyMode}" @click="onModifyClick">정보수정</button>
-          <button id="confirm" type="submit" class="btn btn-primary m-3" v-bind:class="{'d-none' : !modifyMode}">완료</button>
-          <button id="cancel" type="button" class="btn btn-primary m-3" v-bind:class="{'d-none' : !modifyMode}" @click="onCancelClick">취소</button>
+          <button id="modify" type="button" class="btn btn-primary m-3 mt-0" v-bind:class="{'d-none' : modifyMode}" @click="onModifyClick">정보수정</button>
+          <button id="confirm" type="submit" class="btn btn-primary m-3 mt-0" v-bind:class="{'d-none' : !modifyMode}" :disabled="!formIsValid">완료</button>
+          <button id="cancel" type="button" class="btn btn-primary m-3 mt-0" v-bind:class="{'d-none' : !modifyMode}" @click="onCancelClick">취소</button>
         </div>
       </form>
     </div>
